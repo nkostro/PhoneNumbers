d59bf94841f9e8e87a8c93e463501cebc4b31ee6
Nikolai Kostromitin <emailbin@yandex.ru>
2018-12-11 11:42:32 +0500

    Add functionality for phone numbers retrieval.

--- src/main/java/main/PhoneNumbers.java
+++ src/main/java/main/PhoneNumbers.java
@@ -1,26 +1,54 @@
 package main;
 
-import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class PhoneNumbers {
+    private Map<String, List<PhoneNumber>> dataBase;
+
+    public PhoneNumbers() {
+        dataBase = new HashMap<>();
+        fillDataBase();
+    }
+
+    public List<PhoneNumber> getPhoneNumbers(String name) {
+        return dataBase.get(name);
+    }
+
+    private void fillDataBase() {
+        List<PhoneNumber> numbers = new ArrayList<>();
+        numbers.add(new PhoneNumber("+8 800 2000 500"));
+        numbers.add(new PhoneNumber("+8 800 200 600"));
+        dataBase.put("Иванов И.И.", numbers);
+
+        numbers = new ArrayList<>();
+        numbers.add(new PhoneNumber("+8 800 2000 700"));
+        dataBase.put("Петров П.П.", numbers);
+
+        numbers = new ArrayList<>();
+        numbers.add(new PhoneNumber("+8 800 2000 800"));
+        numbers.add(new PhoneNumber("+8 800 2000 900"));
+        numbers.add(new PhoneNumber("+8 800 2000 000"));
+        dataBase.put("Сидоров С.С.", numbers);
+    }
 
     public static void main(String[] args) {
+        System.out.print("Enter name: ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
 
         PhoneNumbers phoneNumbers = new PhoneNumbers();
 
         List<PhoneNumber> numbers = phoneNumbers.getPhoneNumbers(name);
-        for (PhoneNumber num : numbers) {
-            System.out.println(num);
+        if (numbers == null) {
+            System.out.println("No phone numbers for \"" + name + "\" in Data Base");
+        } else {
+            for (PhoneNumber num : numbers) {
+                System.out.println(num);
+            }
         }
-    }
-
-    public List<PhoneNumber> getPhoneNumbers(String name) {
-        List<PhoneNumber> numbers = new ArrayList<>();
-
-        return numbers;
     }
 }
