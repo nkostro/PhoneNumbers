8eafd070578dd71b6f46a533b5767d263de6b6b1
Nikolai Kostromitin <emailbin@yandex.ru>
2018-12-11 10:59:10 +0500

    Add class for phone number and basic functionality.

--- src/main/java/main/PhoneNumbers.java
+++ src/main/java/main/PhoneNumbers.java
@@ -1,6 +1,26 @@
 package main;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class PhoneNumbers {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        PhoneNumbers phoneNumbers = new PhoneNumbers();
+
+        List<PhoneNumber> numbers = phoneNumbers.getPhoneNumbers(name);
+        for (PhoneNumber num : numbers) {
+            System.out.println(num);
+        }
+    }
+
+    public List<PhoneNumber> getPhoneNumbers(String name) {
+        List<PhoneNumber> numbers = new ArrayList<>();
+
+        return numbers;
     }
 }
