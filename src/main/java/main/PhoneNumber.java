3fe3c57a26d52a7418ab04a1ede2695a6b5c2700
Nikolai Kostromitin <emailbin@yandex.ru>
2018-12-11 11:40:43 +0500

    Add phone number's functionality.

--- src/main/java/main/PhoneNumber.java
+++ src/main/java/main/PhoneNumber.java
@@ -1,4 +1,31 @@
 package main;
 
+/**
+ * Phone number representation.
+ */
 public class PhoneNumber {
+    String number;
+
+    /**
+     * Create phone number from given String.
+     */
+    public PhoneNumber(String number) {
+        this.number = new String(number);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        PhoneNumber o = (PhoneNumber)other;
+        return number.equals(o.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return number.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return number;
+    }
 }
